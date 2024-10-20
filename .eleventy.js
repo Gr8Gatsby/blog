59c9eca5df137bd444e33e207b8aa4ccc22bdb96
Gr8Gatsby <kevinhill@live.com>
2024-10-20 17:21:59 +0200

    Setup Eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,8 @@
 module.exports = {
-    dir: {
-      input: "src", // Directory where your content files will be stored
-      output: "_site", // Directory where the generated site will be output
-      includes: "_includes", // Optional: folder for layouts, partials, etc.
-      layouts: "_layouts" // Optional: folder for reusable layouts
-    }
-  };
+  dir: {
+    input: "src", // Folder where your content lives
+    output: "docs", // GitHub Pages will use this folder for serving
+    includes: "_includes", // Optional: for layouts, partials, etc.
+    layouts: "_layouts" // Optional: for reusable layouts
+  }
+};
